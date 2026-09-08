ec2adb7783f402899a34a7fa792cf10e0b609874
MAKO <oosaka0123@gmail.com>
2026-09-08 15:12:25 +0900

    data: add Minami Izuo route 71 verified calendars (#143)

--- collector/tests/test_bus_vision_evidence_registry.py
+++ collector/tests/test_bus_vision_evidence_registry.py
@@ -18,7 +18,7 @@ def registry_document():
 class EvidenceRegistryTest(unittest.TestCase):
     def test_committed_registry_loads_verified_targets(self):
         entries = load_evidence_registry()
-        self.assertEqual(len(entries), 12)
+        self.assertEqual(len(entries), 14)
 
         tsurumachi3 = next(item for item in entries if item.stop_name == "鶴町三丁目")
         self.assertEqual(tsurumachi3.stop_cd, "811")
@@ -68,6 +68,15 @@ class EvidenceRegistryTest(unittest.TestCase):
         self.assertEqual(by_pole["90"].str_line_list, "71-1-1")
         self.assertEqual(by_pole["80"].stop_cd, "797")
         self.assertEqual(by_pole["80"].str_line_list, "55-2-1_71-2-1_90-2-1_91-2-1_94-2-1")
+
+        minami_izuo = [item for item in entries if item.stop_name == "南泉尾"]
+        self.assertEqual(len(minami_izuo), 2)
+        by_pole = {item.pole_cd: item for item in minami_izuo}
+        self.assertEqual(set(by_pole), {"80", "90"})
+        self.assertEqual(by_pole["90"].stop_cd, "1079")
+        self.assertEqual(by_pole["90"].str_line_list, "71-1-1")
+        self.assertEqual(by_pole["80"].stop_cd, "1079")
+        self.assertEqual(by_pole["80"].str_line_list, "55-2-1_71-2-1_80-2-1_90-2-1_91-2-1_94-2-1")
 
         eirakubashisuji = [item for item in entries if item.stop_name == "永楽橋筋"]
         self.assertEqual(len(eirakubashisuji), 2)
