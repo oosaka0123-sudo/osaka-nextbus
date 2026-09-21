603aaf76664c36a9a01338900de7641782bb8f0f
oosaka0123-sudo <oosaka0123@gmail.com>
2026-09-21 19:47:21 +0900

    feat: 音色・音量選択を追加して黒白丸選択を撤去 (#242)
    * feat: add sound and volume buttons; remove mark selector
    
    * fix: make audio initialize reliably and add presets
    
    * feat: style audio selection buttons
    
    * chore: bump alarm cache to v59

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * 時刻表の本体と追加分の結合は js/timetable-loader.js が担当するため、
  * Service Worker は各ファイルを独立してキャッシュするだけにする。
  */
-const CACHE_VERSION = "v58";
+const CACHE_VERSION = "v59";
 const CACHE_NAME = `osaka-nextbus-${CACHE_VERSION}`;
 
 const PRECACHE_URLS = [
@@ -17,21 +17,21 @@ const PRECACHE_URLS = [
   "./alarm.html",
   "./crew-alarm.html",
   "./duty-alarm.html",
-  "./duty-alarm.html?v=58",
+  "./duty-alarm.html?v=59",
   "./alarm.html?v=54",
   "./manifest.json",
   "./css/style.css",
   "./css/alarm.css",
   "./css/crew-alarm.css",
   "./css/duty-alarm.css",
-  "./css/duty-alarm.css?v=58",
+  "./css/duty-alarm.css?v=59",
   "./css/alarm.css?v=54",
   "./js/menu.js",
   "./js/feedback.js",
   "./js/alarm.js",
   "./js/crew-alarm.js",
   "./js/duty-alarm.js",
-  "./js/duty-alarm.js?v=58",
+  "./js/duty-alarm.js?v=59",
   "./js/alarm.js?v=54",
   "./js/timetable-loader.js",
   "./js/app.js",
